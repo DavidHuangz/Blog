5c96e5510e4026eaa0cd37631e8748f509400888
David Huang <stonghk@hotmail.com>
2021-11-11 16:35:10 +1300

    modal form edit

---
+++ frontend/src/components/Form/FormModal.js
@@ -0,0 +1,176 @@
+import React, {useState, useEffect} from 'react';
+import {TextField, IconButton, Typography} from '@material-ui/core';
+import {useDispatch, useSelector} from 'react-redux';
+import FileBase from 'react-file-base64';
+import {createPost, updatePost} from '../../actions/posts';
+import {makeStyles} from '@material-ui/core/styles';
+import {Box} from '@mui/system';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: 100,
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  fileInput: {
+    width: '97%',
+    margin: '10px 0',
+  },
+  FormButtons: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+  },
+}));
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '50%',
+  bgcolor: 'white',
+  border: '1px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const Form = ({currentId, setCurrentId}) => {
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+    selectedFile: '',
+  });
+  const post = useSelector((state) =>
+    currentId ? state.posts.find((message) => message._id === currentId) : null
+  );
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
+
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({
+      creator: '',
+      title: '',
+      message: '',
+      tags: '',
+      selectedFile: '',
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // if (currentId === 0) {
+    dispatch(createPost(postData));
+    //   clear();
+    // } else {
+    //   dispatch(updatePost(currentId, postData));
+    //   clear();
+    // }
+  };
+
+  return (
+    <Box sx={style}>
+      <div className={classes.paper}>
+        <form
+          autoComplete='off'
+          noValidate
+          className={`${classes.root} ${classes.form}`}
+          onSubmit={handleSubmit}
+        >
+          <Typography variant='h6'>
+            {currentId ? `Editing "${post.title}"` : 'Creating a Memory'}
+          </Typography>
+          <TextField
+            name='creator'
+            variant='outlined'
+            label='Creator'
+            fullWidth
+            value={postData.creator}
+            onChange={(e) =>
+              setPostData({...postData, creator: e.target.value})
+            }
+          />
+          <TextField
+            name='title'
+            variant='outlined'
+            label='Title'
+            fullWidth
+            value={postData.title}
+            onChange={(e) => setPostData({...postData, title: e.target.value})}
+          />
+          <TextField
+            name='message'
+            variant='outlined'
+            label='Message'
+            fullWidth
+            multiline
+            rows={4}
+            value={postData.message}
+            onChange={(e) =>
+              setPostData({...postData, message: e.target.value})
+            }
+          />
+          <TextField
+            name='tags'
+            variant='outlined'
+            label='Tags (coma separated)'
+            fullWidth
+            value={postData.tags}
+            onChange={(e) =>
+              setPostData({...postData, tags: e.target.value.split(',')})
+            }
+          />
+          <div className={classes.fileInput}>
+            <FileBase
+              type='file'
+              multiple={false}
+              onDone={({base64}) =>
+                setPostData({...postData, selectedFile: base64})
+              }
+            />
+          </div>
+          <div className={classes.FormButtons}>
+            <IconButton
+              className={classes.buttonSubmit}
+              variant='contained'
+              color='primary'
+              size='large'
+              type='submit'
+            >
+              Submit
+            </IconButton>
+            <IconButton
+              variant='contained'
+              color='secondary'
+              size='small'
+              onClick={clear}
+            >
+              Clear
+            </IconButton>
+          </div>
+        </form>
+      </div>
+    </Box>
+  );
+};
+
+export default Form;
--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -9,10 +9,13 @@ import {
   Typography,
   Card,
   IconButton,
+  Modal,
 } from '@mui/material';
 import reptile from './reptile.jpg';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+
+import FormModal from '../Form/FormModal';
 
 var UseStyles = makeStyles({
   Box: {
@@ -21,37 +24,58 @@ var UseStyles = makeStyles({
   settingsButton: {marginLeft: 190},
 });
 
-function Posts() {
+function Posts({}) {
   const classes = UseStyles();
+
+  // Modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   //redux
   const posts = useSelector((state) => state.posts);
   console.log(posts);
 
   return (
-    <Card className={classes.Box} sx={{maxWidth: 345}}>
-      <CardMedia component='img' height='140' img src={reptile} alt='reptile' />
-      <CardContent>
-        <Typography gutterBottom variant='h5' component='div'>
-          Lizard
-        </Typography>
-        <Typography variant='body2' color='text.secondary'>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size='small'>Like (1)</Button>
-        <div className={classes.settingsButton}>
-          <IconButton size='small'>
-            <DeleteIcon />
-          </IconButton>
-          <IconButton aria-label='settings'>
-            <EditIcon />
-          </IconButton>
-        </div>
-      </CardActions>
-    </Card>
+    <div>
+      <Card className={classes.Box} sx={{maxWidth: 345}}>
+        <CardMedia
+          component='img'
+          height='140'
+          img
+          src={reptile}
+          alt='reptile'
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='div'>
+            Lizard
+          </Typography>
+          <Typography variant='body2' color='text.secondary'>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size='small'>Like </Button>
+          <div className={classes.settingsButton}>
+            <IconButton size='small'>
+              <DeleteIcon />
+            </IconButton>
+            <IconButton aria-label='settings' onClick={handleOpen}>
+              <EditIcon />
+            </IconButton>
+          </div>
+        </CardActions>
+      </Card>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <FormModal />
+      </Modal>
+    </div>
   );
 }
 
