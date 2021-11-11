14288694f237a9edf3e21717986bb756eafb4b09
David Huang <stonghk@hotmail.com>
2021-11-12 02:25:11 +1300

    make it better

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello to DaveBlogger API');
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
